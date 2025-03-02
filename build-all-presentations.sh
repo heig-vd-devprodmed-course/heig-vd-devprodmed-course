a634b9207017ef2a41efeb8a074ca6fc5fc949ad
Vincent <vincent.guidoux@gmail.com>
2025-03-02 23:53:36 +0000

    refactor build-all-presentations.sh to improve PDF conversion and renaming logic

--- build-all-presentations.sh
+++ build-all-presentations.sh
@@ -24,29 +24,34 @@ else
     MARP_CMD="docker run --rm --entrypoint=\"marp-cli.js\" --volume=\"$WORKDIR\":/home/marp/app $MARP_DOCKER_IMAGE"
 fi
 
-PANDOC_CMD="docker run --rm --volume \"$WORKDIR:/data\" --user $(id -u):$(id -g) $PANDOC_DOCKER_IMAGE --from=gfm --template eisvogel --listings -V linkcolor=blue --highlight-style espresso"
+PANDOC_CMD="docker run --rm --volume \"$WORKDIR:/data\" --user $(id -u):$(id -g) $PANDOC_DOCKER_IMAGE --from=gfm --template eisvogel -V linkcolor=blue --highlight-style tango -V monofont='Courier New'"
 
 # Convert support de cours to PDF
 echo "Converting support de cours to PDF..."
-find . -mindepth 3 -maxdepth 3 -type f -name "SUPPORT_DE_COURS.md"  -exec sh -c '
+find . -mindepth 3 -maxdepth 3 -type f -name "SUPPORT_DE_COURS.md" -exec sh -c '
     for file in "$@"; do
         echo "Processing $file..."
         '"$PANDOC_CMD"' -o "$(dirname "$file")/SUPPORT_DE_COURS.pdf" "$file"
     done
 ' sh {} +
+
 # Convert exercices to PDF
 echo "Converting exercices to PDF..."
-find . -mindepth 3 -maxdepth 3 -type f -name "EXERCICES.md" -exec sh -c "
-    echo 'Processing \$1...' && \
-    $PANDOC_CMD -o \"\$(dirname \"\$1\")/EXERCICES.pdf\" \"\$1\"
-" bash {} +
+find . -mindepth 3 -maxdepth 3 -type f -name "EXERCICES.md" -exec sh -c '
+    for file in "$@"; do
+        echo "Processing $file..."
+        '"$PANDOC_CMD"' -o "$(dirname "$file")/EXERCICES.pdf" "$file"
+    done
+' sh {} +
 
 # Convert solutions to PDF
 echo "Converting solutions to PDF..."
-find . -mindepth 3 -maxdepth 3 -type f -name "SOLUTIONS.md" -exec sh -c "
-    echo 'Processing \$1...' && \
-    $PANDOC_CMD -o \"\$(dirname \"\$1\")/SOLUTIONS.pdf\" \"\$1\"
-" bash {} +
+find . -mindepth 3 -maxdepth 3 -type f -name "SOLUTIONS.md" -exec sh -c '
+    for file in "$@"; do
+        echo "Processing $file..."
+        '"$PANDOC_CMD"' -o "$(dirname "$file")/SOLUTIONS.pdf" "$file"
+    done
+' sh {} +
 
 # Convert presentations
 echo "Converting presentations to HTML..."
@@ -56,23 +61,18 @@ echo "Converting presentations to PDF..."
 eval "$MARP_CMD --parallel $(nproc) --pdf **/*-presentation/README.md **/*-quiz/README.md"
 
 # Rename files
-echo "Renaming EXERCICES files to match grandparent directory with 'exercices' suffix..."
-find . -mindepth 3 -maxdepth 3 -type f -name "EXERCICES.pdf" -exec sh -c '
-    chapter_name=$(basename "$(dirname "$(dirname "$1")")")
-    mv "$1" "$(dirname "$1")/$chapter_name-exercices.pdf"
-' sh {} \;
+echo "Renaming generated PDFs..."
+find . -mindepth 3 -maxdepth 3 -type f \( -name "EXERCICES.pdf" -o -name "SUPPORT_DE_COURS.pdf" -o -name "SOLUTIONS.pdf" \) -exec sh -c '
+    for file in "$@"; do
+        chapter_name=$(basename "$(dirname "$(dirname "$file")")")
+        case "$file" in
+            *EXERCICES.pdf) mv "$file" "$(dirname "$file")/$chapter_name-exercices.pdf" ;;
+            *SUPPORT_DE_COURS.pdf) mv "$file" "$(dirname "$file")/$chapter_name-support-de-cours.pdf" ;;
+            *SOLUTIONS.pdf) mv "$file" "$(dirname "$file")/$chapter_name-solutions.pdf" ;;
+        esac
+    done
+' sh {} +
 
-echo "Renaming SUPPORT_DE_COURS files to match grandparent directory with 'support-de-cours' suffix..."
-find . -mindepth 3 -maxdepth 3 -type f -name "SUPPORT_DE_COURS.pdf" -exec sh -c '
-    chapter_name=$(basename "$(dirname "$(dirname "$1")")")
-    mv "$1" "$(dirname "$1")/$chapter_name-support-de-cours.pdf"
-' sh {} \;
-
-echo "Renaming SOLUTIONS files to match grandparent directory with 'solutions' suffix..."
-find . -mindepth 3 -maxdepth 3 -type f -name "SOLUTIONS.pdf" -exec sh -c '
-    chapter_name=$(basename "$(dirname "$(dirname "$1")")")
-    mv "$1" "$(dirname "$1")/$chapter_name-solutions.pdf"
-' sh {} \;
 
 echo "Renaming HTML files to 'index.html'..."
 find . -mindepth 3 -maxdepth 3 -type f -name "README.html" -exec sh -c '
